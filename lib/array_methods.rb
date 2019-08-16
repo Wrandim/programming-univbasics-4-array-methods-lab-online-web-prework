5fb6f80e74c23a2b8e364cc55e941709d7023d13
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-16 18:30:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,7 @@
 
 
 def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element1 = "wow"
-  element2 = "Tardis"
-
-  p array.include?(element1)
-    
-  p array.include?(element2)
+  array.include?(element1)
 end
 
 def using_sort(array)
