5e333208dfbbc11cade50df3cbb15290588ad520
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-16 18:15:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
 
 def using_include(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   element1 = "wow"
   element2 = "Tardis"
 
